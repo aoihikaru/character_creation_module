10fb971989dcbe8bd041fd89ef96758c8a26f2eb
Рита Ярёменко <aoi.hikaru13@yandex.ru>
2023-02-06 00:00:55 +0300

    Добавлена логика метода defence

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from random import randint
 
 DEFAULT_ATTACK = 5
+DEFAULT_DEFENCE = 10
 
 
 class Character:
     RANGE_VALUE_ATTACK = (1, 3)
+    RANGE_VALUE_DEFENCE = (1, 5)
 
     def __init__(self, name):
         self.name = name
@@ -14,7 +16,8 @@ class Character:
         return (f'{self.name} нанёс противнику урон, равный {value_attack}')
 
     def defence(self):
-        pass
+        value_defence = DEFAULT_DEFENCE + randint(*self.RANGE_VALUE_DEFENCE)
+        return (f'{self.name} блокировал {value_defence} урона')
 
     def special(self):
         pass
