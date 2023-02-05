b1bafa822ca50be2ae7e561ed8e3b3b9f92883ef
Рита Ярёменко <aoi.hikaru13@yandex.ru>
2023-02-05 23:40:41 +0300

    Добавлена базовая структура родительского класса Character

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 class Character():
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
+    
+    def attack(self):
+        pass
+
+    def defence(self):
+        pass
+
+    def special(self):
         pass
 
 class Warrior(Character):
     pass
 
+
 class Mage(Character):
     pass
+
 
 class Healer(Character):
     pass
