f24c05ad599d695beb23403dad85c366672299c0
Рита Ярёменко <aoi.hikaru13@yandex.ru>
2023-02-05 23:53:55 +0300

    Добавлена логика метода attack

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-class Character():
+from random import randint
+
+DEFAULT_ATTACK = 5
+
+
+class Character:
+    RANGE_VALUE_ATTACK = (1, 3)
+
     def __init__(self, name):
         self.name = name
-    
+
     def attack(self):
-        pass
+        value_attack = DEFAULT_ATTACK + randint(*self.RANGE_VALUE_ATTACK)
+        return (f'{self.name} нанёс противнику урон, равный {value_attack}')
 
     def defence(self):
         pass
 
     def special(self):
         pass
+
 
 class Warrior(Character):
     pass
