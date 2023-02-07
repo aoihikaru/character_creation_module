130ab23740b89161927da248c295ddc1054ef1e1
Рита Ярёменко <aoi.hikaru13@yandex.ru>
2023-02-07 22:08:06 +0300

    Добавлена функция start_training, а также возвращены импорт модуля run_screensaver и вызов функции

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
+
 DEFAULT_ATTACK = 5
 DEFAULT_DEFENCE = 10
 DEFAULT_STAMINA = 80
@@ -32,7 +34,7 @@ class Character:
 
 
 class Warrior(Character):
-    BRIEF_DESC_CHAR_CLASS = ('дерзкий воин ближнего боя. '
+    BRIEF_DESC_CHAR_CLASS = (' дерзкий воин ближнего боя. '
                              'Сильный, выносливый и отважный')
     RANGE_VALUE_ATTACK = (3, 5)
     RANGE_VALUE_DEFENCE = (5, 10)
@@ -58,13 +60,30 @@ class Healer(Character):
     SPECIAL_SKILL = 'Защита'
 
 
+def start_training(character):
+    print('Потренируйся управлять своими навыками.')
+    print('Введи одну из команд: attack — чтобы атаковать противника, '
+          'defence — чтобы блокировать атаку противника или '
+          'special — чтобы использовать свою суперсилу.')
+    print('Если не хочешь тренироваться, введи команду skip.')
+    cmd = None
+    command_definition = {
+        'attack': character.attack,
+        'defence': character.defence,
+        'special': character.special
+    }
+    while cmd != 'skip':
+        cmd = input('Введи команду: ')
+        if cmd in command_definition:
+            print(command_definition[cmd]())
+    return 'Тренировка окончена.'
+
+
 def choice_char_class(char_name: str) -> Character:
     """
     Возвращает строку с выбранным
     классом персонажа.
     """
-    # Добавили словарь,
-    # в котором соотносится ввод пользователя и класс персонажа.
     game_classes = {'warrior': Warrior, 'mage': Mage, 'healer': Healer}
 
     approve_choice: str = None
@@ -74,9 +93,21 @@ def choice_char_class(char_name: str) -> Character:
                                'за которого хочешь играть: Воитель — warrior, '
                                'Маг — mage, Лекарь — healer: ')
         char_class: Character = game_classes[selected_class](char_name)
-        # Вывели в терминал описание персонажа.
         print(char_class)
         approve_choice = input('Нажми (Y), чтобы подтвердить выбор, '
                                'или любую другую кнопку, '
                                'чтобы выбрать другого персонажа ').lower()
     return char_class
+
+
+if __name__ == '__main__':
+    run_screensaver()
+    print('Приветствую тебя, искатель приключений!')
+    print('Прежде чем начать игру...')
+    char_name: str = input('...назови себя: ')
+    print(f'Здравствуй, {char_name}! '
+          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
+    print('Ты можешь выбрать один из трёх путей силы:')
+    print('Воитель, Маг, Лекарь')
+    char_class: str = choice_char_class(char_name)
+    print(start_training(char_class))
